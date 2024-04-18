cda5495a0bd9eb2b1a63d46ffb216703b2bd4b76
Daniil <237di.5683.1@gmail.com>
2024-04-18 15:51:02 +0300

    dzzz

--- Assets/_Platformer/_Workspace/YOUR_NAME/Scripts/Interaction/Interactor.cs
+++ Assets/_Platformer/_Workspace/YOUR_NAME/Scripts/Interaction/Interactor.cs
@@ -2,38 +2,23 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
-public class Interactor : MonoBehaviour
+public class Character : MonoBehaviour, IDamageable
 {
-    private List<IInteractive> _interactiveObjects = new();
+   //...
+   private void SetupInputManager(IInputManager inputManager)
+   {
+      //...
+      inputManager.onInteract += OnInteract;
+   }
+   private void RemoveInputManager(IInputManager inputManager)
+   {
+      //...
+      inputManager.onInteract -= OnInteract;
+   }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        var interactive = other.GetComponent<IInteractive>();
-        if (interactive != null) _interactiveObjects.Add(interactive);
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        var interactive = other.GetComponent<IInteractive>();
-        if (interactive != null) _interactiveObjects.Remove(interactive);
-    }
-    
-    private void Sort()
-    {
-        if (!HasInteractions()) return;
-        _interactiveObjects = _interactiveObjects.OrderBy(x => x.Priority).ToList();
-    }
-    
-    public void Interact()
-    {
-        Sort();
-        var interactive = _interactiveObjects.LastOrDefault(x => x.CanInteractWith(this));
-        interactive?.Interact(this);
-    }
-
-    public bool HasInteractions()
-    {
-        return _interactiveObjects.Count(x => x.CanInteractWith(this)) > 0;
-    }
+   private void OnInteract()
+   {
+      interactor.Interact();
+   }
 }
--- Assets/_Platformer/_Workspace/YOUR_NAME/Scripts/Triggers/Trigger.cs
+++ Assets/_Platformer/_Workspace/YOUR_NAME/Scripts/Triggers/Trigger.cs
@@ -5,53 +5,46 @@ using UnityEngine;
 [Serializable]
 public enum TriggerType
 {
-    Enter,
-    Exit,
-    Interactive
+   Enter, Exit, Interactive
 }
-
 [RequireComponent(typeof(Collider2D))]
 public abstract class Trigger : MonoBehaviour, IInteractive
 {
-    [Header("Trigger")]
-    [SerializeField] private TriggerType type;
-    [SerializeField] private int priority = -1;
-    [SerializeField] private string[] tags = { "Player" };
-    [SerializeField] private bool once;
-    
-    private bool _done;
+   [Header("Trigger")]
+   [SerializeField] private TriggerType type;
+   [SerializeField] private int priority = -1;
+   [SerializeField] private string[] tags = { "Player" };
+   [SerializeField] private bool once;
 
-    public int Priority => priority;
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!tags.Contains(other.tag)) return;
-        if (type != TriggerType.Enter || _done) return;
-        _done = once;
-        Activate(other);
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (!tags.Contains(other.tag)) return;
-        if (type != TriggerType.Exit || _done) return;
-        _done = once;
-        Activate(other);
-    }
-    
-    public abstract void Activate(Collider2D other);
-    
-    public void Interact(Interactor instigator)
-    {
-        if (!CanInteractWith(instigator)) return;
-        _done = once;
-        Activate(instigator.GetComponent<Collider2D>());
-    }
+   private bool _done;
+   public int Priority => priority;
 
-    public bool CanInteractWith(Interactor instigator)
-    {
-        return tags.Contains(instigator.tag) 
-               && type == TriggerType.Interactive 
-               && !_done;
-    }
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+       if (!tags.Contains(other.tag)) return;
+       if (type != TriggerType.Enter || _done) return;
+       _done = once;
+       Activate(other);
+   }
+   private void OnTriggerExit2D(Collider2D other)
+   {
+       if (!tags.Contains(other.tag)) return;
+       if (type != TriggerType.Exit || _done) return;
+       _done = once;
+       Activate(other);
+   }
+   public void Interact(Interactor instigator)
+   {
+       if (!CanInteractWith(instigator)) return;
+       _done = once;
+       Activate(instigator.GetComponent<Collider2D>());
+   }
+   public bool CanInteractWith(Interactor instigator)
+   {
+       return tags.Contains(instigator.tag)
+              && type == TriggerType.Interactive && !_done;
+   }
+   public abstract void Activate(Collider2D other);
 }
